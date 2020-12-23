368e243039d91952e34d70808996f7c0c5235feb
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-23 21:54:45 +0000

    Added dictionary for compilation with root

---
+++ dict/ToyGenDict.h
@@ -0,0 +1,10 @@
+#ifndef DICT_TOYGENDICT_H
+#define DICT_TOYGENDICT_H 1
+// Include files
+
+#include "event.h"
+#include "generator.h"
+#include "efficiency.h"
+#include "amplitude.h"
+
+#endif // DICT_TOYGEN_H
