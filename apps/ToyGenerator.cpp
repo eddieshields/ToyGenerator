7e126ae33181a19a4f0215a70742614c1daa3aeb
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-23 21:54:14 +0000

    Added executable

---
+++ apps/ToyGenerator.cpp
@@ -0,0 +1,32 @@
+#include "event.h"
+#include "generator.h"
+#include "efficiency.h"
+#include "amplitude.h"
+#include "configuration.h"
+#include "algorithm.h"
+
+#include <iostream>
+
+int main()
+{
+
+  Event ev;
+  Generator gen("Generator",1.9,0.4,0.4,0.5);
+  Efficiency eff("Efficiency");
+  Efficiency eff1("Efficiency1");
+  Efficiency eff2("Efficiency2");
+  Efficiency eff3("Efficiency3");
+  Amplitude amp("Amplitude");
+
+  Configuration flow(eff,eff1,eff2,amp,eff3);
+
+  Algorithm* head = flow.head;
+  head->operator()(ev);
+  /*
+  while ( head != nullptr ) {
+      head->operator()(ev);
+  }
+  */
+  std::cout << "Pdf = " << ev.pdf << std::endl;
+  return 0;
+}
