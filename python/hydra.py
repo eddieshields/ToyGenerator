0203b73bf7c27b58ce757dec9edcc8d063c9f905
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-24 17:19:26 +0000

    Added python wrapper around Hydra class

---
+++ python/hydra.py
@@ -0,0 +1,16 @@
+import ROOT as r 
+
+r.gSystem.Load('build/libHYDRA')
+
+# Wrapper class for C++ Hydra class.
+class Hydra:
+    def __init__(self):
+        self.hy = r.Hydra()
+
+    def __call__(self):
+        return self.hy.m_configuration
+
+
+    
+
+
