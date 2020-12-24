ebc2f1010937fac384ac3dcae80b9802d4ca29f8
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-24 17:18:06 +0000

    Added hydra to root compilation

--- dict/ToyGenDict.h
+++ dict/ToyGenDict.h
@@ -2,9 +2,11 @@
 #define DICT_TOYGENDICT_H 1
 // Include files
 
+#include "hydra.h"
 #include "event.h"
 #include "generator.h"
 #include "efficiency.h"
 #include "amplitude.h"
+#include "particle.h"
 
 #endif // DICT_TOYGEN_H
--- dict/ToyGenLinkDef.h
+++ dict/ToyGenLinkDef.h
@@ -5,9 +5,11 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+#pragma link C++ class Hydra+;
 #pragma link C++ class Event+;
 #pragma link C++ class Generator+;
 #pragma link C++ class Efficiency+;
 #pragma link C++ class Amplitude+;
+#pragma link C++ class Particle+;
 
 #endif
