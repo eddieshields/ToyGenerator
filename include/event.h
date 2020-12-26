a9be6ff3b5e232efca6163fbec90cdbb760db29b
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-26 23:48:09 +0000

    Structural changes to event model. Needs a lot more work

--- include/event.h
+++ include/event.h
@@ -22,11 +22,23 @@ public:
   double efficiency = {1};
   bool   Accept = {false};
 
-  double    operator[](std::string name) { return m_v[name]; }
-  Particle& operator()(std::string name) { return m_particles[name]; }
-private:
-  std::map<std::string,Particle> m_particles;
+  double&    operator[](std::string name) { return m_v[name]; }
+  Particle& operator()(int index ) { return m_particles[index]; }
+
+  std::vector<Particle> m_particles;
+  //std::map<std::string,Particle> m_particles;
   std::map<std::string,double>   m_v;
+
+  void updateMasses()
+  {  
+    TLorentzVector m12 = m_particles[0].momentum() + m_particles[1].momentum();
+    TLorentzVector m13 = m_particles[0].momentum() + m_particles[2].momentum();
+    TLorentzVector m23 = m_particles[1].momentum() + m_particles[2].momentum();
+  
+    m_v["mSq12"] = m12.M2();
+    m_v["mSq13"] = m13.M2();
+    m_v["mSq23"] = m23.M2();
+  }
 };
 
 #endif
