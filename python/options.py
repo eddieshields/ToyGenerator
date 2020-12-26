841efb414bbc63930fe165a6f6b1fb92c9a4aaea
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-26 18:29:48 +0000

    Updated options

--- python/options.py
+++ python/options.py
@@ -5,14 +5,18 @@ hy = Hydra()
 gen = r.Generator("Generator",1.9,0.4,0.4,0.5)
 eff = r.Efficiency("Efficiency")
 amp = r.Amplitude("Amplitude")
+acc = r.Accept("Accept")
 # Create sequence.
 seq = r.Sequence()
 seq.addAlgorithm(gen)
 seq.addAlgorithm(eff)
 seq.addAlgorithm(amp)
+seq.addAlgorithm(acc)
 seq.printAlgorithmSequence()
 
 # Configure Hydra.
 hy().EvtMax = 10000
 hy().TreeName = "d0kshh"
 hy().AlgoSequence = seq
+
+hy.run()
