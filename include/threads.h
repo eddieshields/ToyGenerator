9464433149cca6befa03a39106628fad445167f5
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-26 21:59:30 +0000

    Cleaned up threading for jobs

--- include/threads.h
+++ include/threads.h
@@ -1,6 +1,8 @@
 #ifndef TOYGEN_THREADS_H
 #define TOYGEN_THREADS_H
 
+#include "msgservice.h"
+
 #include<thread>
 #include <pthread.h>
 #include <vector>
@@ -8,29 +10,24 @@
 class Threads 
 {
 public:
-  Threads() {};
-  ~Threads() {};
   template<typename FUNC, typename... IN_TYPES>
-  void operator()(FUNC& function, IN_TYPES... args)
+  Threads(FUNC& function,int& nthreads)
   {
-    m_threads.resize( m_nthreads );
-    for (unsigned int i = 0; i < m_nthreads; i++) {
-      m_threads.push_back( std::thread(function,args...) );
+    INFO("Will use "+std::to_string(nthreads)+" threads");
+    for (int i = 0; i < nthreads; i++) {
+      std::thread t(function);
+      m_threads.push_back(std::move(t));
     }
-
-    for (unsigned int i = 0; i < m_nthreads; i++) {
-      m_threads[i].join();
+    for (auto &th : m_threads) {
+      th.join();
     }
-    
   }
 
-  void setNThreads(unsigned int n) { m_nthreads = n; }
+  ~Threads() {};
 
 private:
   std::atomic<bool>        m_complete;
   std::vector<std::thread> m_threads;
-  unsigned int             m_nthreads = {32};
-
 };
 
 #endif
