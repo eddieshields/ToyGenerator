3c224355698d2bad741bccb040600f1ef73e5214
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-26 15:44:18 +0000

    Update script to reflect changes in Sequence

--- python/options.py
+++ python/options.py
@@ -6,7 +6,11 @@ gen = r.Generator("Generator",1.9,0.4,0.4,0.5)
 eff = r.Efficiency("Efficiency")
 amp = r.Amplitude("Amplitude")
 # Create sequence.
-seq = r.Sequence(gen,eff,amp)
+seq = r.Sequence()
+seq.addAlgorithm(gen)
+seq.addAlgorithm(eff)
+seq.addAlgorithm(amp)
+seq.printAlgorithmSequence()
 
 # Configure Hydra.
 hy().EvtMax = 10000
