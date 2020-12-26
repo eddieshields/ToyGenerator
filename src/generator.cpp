39d9aeb07fcdd2afe8c897d4fb415edb263ee39c
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-26 23:49:03 +0000

    Updated momentum generation

--- src/generator.cpp
+++ src/generator.cpp
@@ -6,10 +6,9 @@ void Generator::operator()(Event& ev)
   m_phsp.Generate();
 
   // 
-  for (auto& x : ev.m_particles) {
-    if ( !x.second.isStable() ) continue;
-    TLorentzVector* p = x.second.momentum();
-    p = m_phsp.GetDecay(0);
+  for (int i = 0; i < m_daughters.size(); i++) {
+    TLorentzVector* vec = m_phsp.GetDecay(i);
+    ev.m_particles[i].m_p = *vec;
   }
 
   return;
