374c38bea15ce0a7c86096363147336d2bcd43e0
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-27 14:10:14 +0000

    Add particle creation to generation

--- src/generator.cpp
+++ src/generator.cpp
@@ -2,14 +2,14 @@
 
 void Generator::operator()(Event& ev)
 {
+  // Need to add multithreaded support so we dont need new TGen in each loop.
   // Generate event.
-  m_phsp.Generate();
+  TGenPhaseSpace phsp(m_phsp);
+  phsp.Generate();
 
-  // 
   for (int i = 0; i < m_daughters.size(); i++) {
-    TLorentzVector* vec = m_phsp.GetDecay(i);
-    ev.m_particles[i].m_p = *vec;
+    ev.m_particles.push_back( Particle() );
+    ev.m_particles[i].m_p = *phsp.GetDecay(i);
   }
-
   return;
 }
