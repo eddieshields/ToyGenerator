7e311c32761efa45627d0873570ce21c07002729
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-27 18:12:20 +0000

    Added getter methods to event

--- include/event.h
+++ include/event.h
@@ -20,12 +20,19 @@ public:
 
   double pdf = {1};
   double efficiency = {1};
+  double weight = {1};
   bool   Accept = {false};
 
+  std::vector<Particle>& particles() { return m_particles; }
+  Particle& mother()                 { return m_particles[0]; }
+  Particle& daughter(int index)      { return m_particles[index]; }
+  double* data()                     { makeData(); return m_data.data(); } 
+
   double&    operator[](std::string name) { return m_v[name]; }
-  Particle& operator()(int index ) { return m_particles[index]; }
+  Particle& operator()(int index)         { return m_particles[index]; }
 
   std::vector<Particle> m_particles;
+  std::vector<double>   m_data;
   //std::map<std::string,Particle> m_particles;
   std::map<std::string,double>   m_v;
 
@@ -43,6 +50,21 @@ public:
     m_v["mSq13"] = p13.M2();
     m_v["mSq23"] = p23.M2();
   }
+
+  void makeData()
+  {
+    std::vector<TLorentzVector> pVec;
+    for (auto& p : m_particles) {
+      pVec.push_back( p.momentum() );
+    }
+    m_data.resize( 4 * pVec.size() );
+    for( int i = 0 ; i < 4; ++i ){
+      for( int j = 0 ; j < pVec.size(); ++j ){
+        m_data[ 4 * j + i] = pVec[j][i];
+      }
+    }
+  }
+
 };
 
 #endif
