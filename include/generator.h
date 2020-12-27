1afb79c0ef6a0345d8c7d606cb6a9fc9c0a315ca
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-27 14:09:28 +0000

    Removed constructor

--- include/generator.h
+++ include/generator.h
@@ -4,6 +4,7 @@
 #include "event.h"
 #include "algorithm.h"
 #include "particlestore.h"
+#include "msgservice.h"
 
 #include "TGenPhaseSpace.h"
 #include "TLorentzVector.h"
@@ -33,14 +34,6 @@ public:
     addToDaughters(daughter_names...);
     m_phsp.SetDecay(m_mother,m_daughters.size(),m_daughters.data());
   }
-  Generator(std::string name, const double mM, const double m1, const double m2, const double m3) :
-    Algorithm(name),
-    m_phsp()
-  {
-    TLorentzVector m_mother(0.,0.,0.,mM);
-    std::vector<double> m_daughters = {m1,m2,m3};
-    m_phsp.SetDecay(m_mother,m_daughters.size(),m_daughters.data());
-  }
   ~Generator() {}
   virtual void operator() (Event& ev);
 
@@ -48,6 +41,7 @@ public:
   void addToDaughters(IN_TYPE input, IN_TYPES... inputs) { m_daughters.push_back( input ); addToDaughters(inputs...); }
   template <typename IN_TYPE>
   void addToDaughters(IN_TYPE input) { m_daughters.push_back(input); }
+
   template <typename... IN_TYPES>
   void addToDaughter(std::string daughter, IN_TYPES... daughters) { m_daughters.push_back( gParticleStore(daughter,"mass") ); addToDaughters(daughters...); }
   void addToDaughter(std::string daughter) { m_daughters.push_back( gParticleStore(daughter,"mass") ); }
