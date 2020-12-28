8fd94a0e08b5506133231d72e4064d161819df79
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-28 18:32:28 +0000

    Added default constructor

--- include/algorithm.h
+++ include/algorithm.h
@@ -7,14 +7,17 @@
 
 struct Algorithm
 {
-  Algorithm(std::string name) { m_name = name; }
+  Algorithm() : m_name("") {}
+  Algorithm(std::string name) :
+    m_name(name)
+  {}
   virtual ~Algorithm() {};
   
   virtual void operator() (Event& ev) = 0;
   Algorithm* next = {nullptr};
 
-  std::string m_name;
-  std::string name() { return m_name; }
+  const std::string m_name;
+  const std::string name() { return m_name; }
 };
 
 #endif
