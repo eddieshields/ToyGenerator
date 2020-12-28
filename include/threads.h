63ef6c535196c9e78460c49a1b014c847939744f
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-28 15:31:38 +0000

    Moved action into operator

--- include/threads.h
+++ include/threads.h
@@ -9,34 +9,38 @@
 #include <vector>
 #include <future>
 
- #include "TROOT.h"
+#include "TROOT.h"
 
+template<typename FUNC>
 class Threads 
 {
 public:
-  template<typename FUNC, typename... IN_TYPES>
-  Threads(FUNC& function,int& nthreads)
+  Threads(FUNC& function,int& nthreads, int& nevents) :
+    m_function(function),
+    m_nthreads( nthreads )
   {
+    m_list.reserve( nevents );
     ROOT::EnableImplicitMT(nthreads);
-    INFO("Will use "+std::to_string(nthreads)+" threads");
-    for (int i = 0; i < nthreads; i++) {
-      std::future<std::vector<Event>> t = std::async(std::launch::async,function,i);
-      m_threads.push_back( std::move(t) );
-    }
-
-    for (int i = 0; i < nthreads; i++) {
-      std::vector<Event> tmp = m_threads[i].get();
-      for (int j = 0; j < tmp.size(); j++) {
-        Event e = tmp[j];
-        m_list.push_back(std::move(e));
-      }
-    }
-
   }
   ~Threads() {};
 
+  void operator()()
+  {
+    for (int i = 0; i < m_nthreads; i++) {
+      std::future<std::vector<Event>> t = std::async(std::launch::async,m_function,i);
+      m_threads.push_back( std::move(t) );
+    }
+
+    for (int i = 0; i < m_nthreads; i++) {
+      std::vector<Event> tmp = m_threads[i].get();
+      m_list.insert( m_list.end(), tmp.begin(), tmp.end() );
+    }
+  }
+
   std::vector<Event> list() { return m_list; }
 private:
+  FUNC&                                        m_function;
+  const int                                    m_nthreads;
   std::atomic<bool>                            m_complete;
   std::vector<std::future<std::vector<Event>>> m_threads;
   std::vector<Event>                           m_list;
