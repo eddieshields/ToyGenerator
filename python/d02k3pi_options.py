138d6a6f83675887ccab450c9e4cb334fe2631d9
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-28 21:24:45 +0000

    updated options to reflect changes

--- python/d02k3pi_options.py
+++ python/d02k3pi_options.py
@@ -1,23 +1,19 @@
 from hydra import *
 hy = Hydra()
+hy.setDecay("D0 => K+ pi- pi+ pi-")
 
-
-vars = ["pdf","weight","t","K_q"]
 # Configure algorithms.
-gen = r.Generator("Generator",1.8645,0.49368,0.13957,0.13957,0.13957)
-eff = r.Efficiency("Efficiency")
-#amp = r.Amplitude("Amplitude")
+gen = r.Generator("Generator")
 amp = r.D02K3Pi("D02K3Pi")
 acc = r.Accept("Accept")
 acc.setMaxPdf(0.4)
 tup = r.Tupling("Tupling")
-tup.setVariables(vars)
+tup.addMass()
 
 
 # Create sequence.
 seq = r.Sequence()
 seq.addAlgorithm(gen)
-seq.addAlgorithm(eff)
 seq.addAlgorithm(amp)
 seq.addAlgorithm(acc)
 seq.addAlgorithm(tup)
@@ -27,7 +23,7 @@ seq.printAlgorithmSequence()
 hy().EvtMax = 100000
 hy().TreeName = "d02kshh"
 hy().AlgoSequence = seq
-hy().Variables = vars
+hy().Variables = tup.getVariables()
 hy().NThreads = 1
 hy().OutputLocation = "/Users/eddieshields/Documents/LHCb/ToyGenerator/build/tmp/output.root"
 
