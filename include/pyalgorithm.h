2dce19c3674137e9d6860b50d9731945d12a3b5a
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-29 21:44:30 +0000

    Tidyup

--- include/pyalgorithm.h
+++ include/pyalgorithm.h
@@ -9,19 +9,16 @@
 class PyAlgorithm : public Algorithm
 {
 public:
-  PyAlgorithm(std::string name,std::function<void(Event&)>& func) : 
+  PyAlgorithm(std::string name,std::function<void(Event&)> func) : 
     Algorithm(name),
     m_func( func )
-  {
-    Event ev;
-    m_func(ev);
-  }
+  {}
   ~PyAlgorithm() {}
 
-  virtual void operator()(Event& ev) { INFO("IN OPERATOR"); m_func(ev); } 
+  virtual void operator()(Event& ev) { m_func(ev); } 
 
 private:
-  std::function<void(Event&)>& m_func;
+  std::function<void(Event&)> m_func;
 };
 
 #endif
