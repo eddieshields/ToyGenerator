caddc92fcd23b78e62fba31d8800e0e5afc9bb4a
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-29 22:52:15 +0000

    Updated options

--- python/d02k3pi_options.py
+++ python/d02k3pi_options.py
@@ -2,13 +2,15 @@ from hydra import *
 hy = Hydra()
 hy.setDecay("D0 => K+ pi- pi+ pi-")
 
+
 def pyfunc(ev):
-    print("Inside python function")
+    ev["mSq12"] = 0.8
+    ev.pdf = 1
 
 # Configure algorithms.
 gen = r.Generator("Generator")
 amp = r.D02K3Pi("D02K3Pi")
-eff = r.PyAlgorithm("Eff",pyfunc)
+eff = r.PyAlgorithm("Eff",pyfunc) 
 acc = r.Accept("Accept")
 acc.setMaxPdf(0.4)
 tup = r.Tupling("Tupling")
@@ -19,13 +21,13 @@ tup.addMass()
 seq = r.Sequence()
 seq.addAlgorithm(gen)
 seq.addAlgorithm(amp)
-seq.addAlgorithm(eff)
+#seq.addAlgorithm(eff)
 seq.addAlgorithm(acc)
 seq.addAlgorithm(tup)
 seq.printAlgorithmSequence()
 
 # Configure Hydra.
-hy().EvtMax = 32
+hy().EvtMax = 10000
 hy().TreeName = "d02kshh"
 hy().AlgoSequence = seq
 hy().Variables = tup.getVariables()
