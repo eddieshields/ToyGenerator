e1d89df791e7c315652a076828f478d4de5210aa
Edward Shields <edward.brendan.shields@cern.ch>
2020-12-29 21:45:37 +0000

    Tidied up class

--- python/hydra.py
+++ python/hydra.py
@@ -1,20 +1,17 @@
 import ROOT as r 
-
 r.gSystem.Load('build/libHYDRA')
 
 # Wrapper class for C++ Hydra class.
 class Hydra:
     def __init__(self):
         self.hy = r.Hydra()
+        
 
     def __call__(self):
         return self.hy.m_configuration
 
     def run(self):
-        self.hy.run()
+         self.hy.run()
 
     def setDecay(self,decay):
         self.hy.setDecay(decay)
-    
-
-
