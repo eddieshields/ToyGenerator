cba1fdadd900847e6e4ca628c99b5971038a758a
Edward Shields <edward.brendan.shields@cern.ch>
2021-01-06 21:34:44 +0000

    Updated ROOT dict

--- dict/ToyGenDict.h
+++ dict/ToyGenDict.h
@@ -2,18 +2,47 @@
 #define DICT_TOYGENDICT_H 1
 // Include files
 
-#include "hydra.h"
-#include "pyalgorithm.h"
-#include "event.h"
-#include "particle.h"
+// Core HYDRA
+#include "algorithm.h"
+#include "algorithmstore.h"
+#include "algorithmwrapper.h"
+#include "clock.h"
 #include "descriptor.h"
-#include "generator.h"
-#include "efficiency.h"
-#include "amplitude.h"
-#include "accept.h"
-#include "tupling.h"
-#include "param.h"
-#include "d02k3pi.h"
+#include "event.h"
+#include "hydra.h"
+#include "particle.h"
+#include "particlestore.h"
+#include "pyalgorithm.h"
+#include "random.h"
+#include "sequence.h"
+#include "threads.h"
 
+// Algorithms.
+#include "accept.h"
+#include "amplitude.h"
+#include "compositeparam.h"
+#include "customparam.h"
+#include "d02k3pi.h"
+#include "decay3body.h"
+#include "decay3bodymixing.h"
+#include "efficiency.h"
+#include "eventparam.h"
+#include "generator.h"
+#include "param.h"
+#include "particleparam.h"
+#include "tupling.h"
+
+// Models.
+#include "angular.h"
+#include "coefficient.h"
+#include "configfile.h"
+#include "configureamplitude.h"
+#include "dalitzamplitude.h"
+#include "dalitzmixing.h"
+#include "flatte.h"
+#include "parameter.h"
+#include "phasespace.h"
+#include "relbreitwigner.h"
+#include "resonance.h"
 
 #endif // DICT_TOYGEN_H
