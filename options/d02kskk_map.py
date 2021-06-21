2f5fb904d697670e1c73b05fb0f81df5f1f18569
Edward Shields <edward.brendan.shields@cern.ch>
2021-06-21 13:21:24 +0200

    Options to generate map with amplitude model

---
+++ options/d02kskk_map.py
@@ -0,0 +1,39 @@
+from hydra import *
+
+import argparse
+parser = argparse.ArgumentParser(description='Submit toy generation jobs to ganga')
+parser.add_argument('-s','--seed',  type=int, default=0,           help='Random seed')
+parser.add_argument('-m','--model', type=str, default='belle2010', help='Model')
+args = parser.parse_args()
+
+
+SetDecay("D0 => K_S0 K+ K-")
+SetSeed(args.seed)
+
+# Configure algorithms.
+# Generate events according to decay.
+gen = Generator("Generator")
+# Set event pdf value according to an amplitude model with mixing.
+amp = Decay3Body("D02KSKKAmplitude","cfg/"+args.model+".cfg")
+# Add variables to be saved in ntuple.
+tup = Tupling("Tupling")
+tup.addPID()
+tup.addEventInfo()
+tup.addCompositeMass()
+
+
+# Create sequence.
+seq = Sequence()
+seq.addAlgorithm(gen)
+seq.addAlgorithm(amp)
+seq.addAlgorithm(tup)
+seq.printAlgorithmSequence()
+
+# Configure Hydra.
+Hydra().EvtMax = 10000
+Hydra().AlgoSequence = seq
+Hydra().Variables = tup.getVariables()
+Hydra().NThreads = -1
+Hydra().TreeName = "d02kskk.root"
+Hydra().OutputLocation = "d02kskk_"+str(args.seed)+".root"
+Hydra.run()
